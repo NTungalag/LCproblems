81e220ab3b6c47f32d4ecc76d6192b81e3dce730
narantungalag <narantungalag.b@novelsoft.mn>
2024-08-27 19:30:03 +0800

    21. Merge Two Sorted Lists

---
+++ n21MergeTwoSortedLists.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+  var pre = new ListNode();
+
+  let head = pre;
+  while (list1 && list2) {
+    if (list1.val <= list2.val) {
+      pre.next = list1;
+      list1 = list1.next;
+    } else {
+      pre.next = list2;
+      list2 = list2.next;
+    }
+    pre = pre.next;
+  }
+  if (list1) pre.next = list1;
+
+  if (list2) pre.next = list2;
+  console.log(JSON.stringify(pre));
+  return head.next;
+};
